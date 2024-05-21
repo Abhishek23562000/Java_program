918cdd710b136f65fdc198299ca6a1bb2a2306bd
Abhishek23562000 <132987366+Abhishek23562000@users.noreply.github.com>
2024-05-21 21:20:09 +0530

    swap_number.java swap number by multiple way
    we make a java program to swap a  number by using various approach

--- swap_number.java
+++ swap_number.java
@@ -2,14 +2,33 @@ package Basic_question;
 
 public class swap_number {
     public static void main(String[] args) {
-        int a = 13;
+      int a = 13;
         int b = 23;
-
+     // using xor bitwise operator
        a = a^b;
        b = a^b;
        a = a^b;
 
         System.out.println(a);
         System.out.println(b);
+
+        int c = 12;
+        int d = 21;
+        
+        //simple taking third variable
+        int temp = c;
+        c = d;
+        d = temp;
+        System.out.println(c);
+        System.out.println(d);
+
+        int e =20;
+        int f =15;
+   // using arithmetic operators
+        e = e+f;
+        f = e-f;
+        e = e-f;
+        System.out.println(e);
+        System.out.println(f);
     }
 }
